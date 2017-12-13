d0782412211b978c7ff5a3c1d73cbe0d062cc5c9
frederikja163 <fjaplv64@gmail.com>
2017-12-13 19:02:24 +0100

    V 0.1
    Added basic character movement
    Added basic network hub
    Added basic world to walk around in

---
+++ 3D survival game/Assets/CharactorMotor.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharactorMotor : MonoBehaviour
+{
+    [HideInInspector]
+    public Vector3 velocity = Vector3.zero;
+    [HideInInspector]
+    public Vector3 rotation = Vector3.zero;
+    [HideInInspector]
+    public float camRotationX = 0;
+
+    private Rigidbody rb = null;
+    [SerializeField]
+    private Transform cam = null;
+
+    //Called when initialized
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    //Called on every physics update
+    private void FixedUpdate()
+    {
+        PerformMovement();
+        PerformRotation();
+    }
+
+    //Performs movement on a physics update
+    private void PerformMovement()
+    {
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    //Performs a movement on a physics update
+    private void PerformRotation()
+    {
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+        if (cam != null)
+        {
+            cam.localEulerAngles += Vector3.right * camRotationX;
+        }
+    }
+}
---
+++ 3D survival game/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(CharactorMotor))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField]
+    private float movementSpeed = 10f;
+    [SerializeField]
+    private float sensitivity = 3f;
+
+    private CharactorMotor motor = null;
+
+    //Called when initialized
+    private void Start()
+    {
+        motor = GetComponent<CharactorMotor>();
+    }
+
+    //Called on every frame
+    private void Update()
+    {
+        UpdateMovement();
+        UpdateRotation();
+    }
+
+    //Updates movement every frame
+    private void UpdateMovement()
+    {
+        Vector3 movX = transform.right * Input.GetAxisRaw("Horizontal");
+        Vector3 movZ = transform.forward * Input.GetAxisRaw("Vertical");
+
+        Vector3 velocity = (movX + movZ).normalized * movementSpeed;
+
+        motor.velocity = velocity;
+    }
+
+    //Updates rotation every frame
+    private void UpdateRotation()
+    {
+        Vector3 rotation = transform.up * Input.GetAxisRaw("Mouse X");
+
+        motor.rotation = rotation * sensitivity;
+
+        motor.camRotationX = -Input.GetAxisRaw("Mouse Y") * sensitivity;
+    }
+}
